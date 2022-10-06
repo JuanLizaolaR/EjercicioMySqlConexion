c1f1263749d8c46fb97b83f5d492ff7a17c6a58e
JuanLizaolaR <115133354+JuanLizaolaR@users.noreply.github.com>
2022-10-06 00:41:06 -0500

    Commit inicial
    Carga inicial Proyecto Data Warehouse

---
+++ ConexionAMYSQL/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ConexionAMYSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txthost = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            // 
+            // txthost
+            // 
+            this.txthost.Location = new System.Drawing.Point(183, 80);
+            this.txthost.Name = "txthost";
+            this.txthost.Size = new System.Drawing.Size(231, 26);
+            this.txthost.TabIndex = 3;
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(183, 142);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(231, 26);
+            this.txtuser.TabIndex = 4;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(183, 209);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(231, 26);
+            this.txtpass.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(234, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(515, 384);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.txthost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Conectar a Base de datos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txthost;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ConexionAMYSQL/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace ConexionAMYSQL
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conexion;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (string.IsNullOrEmpty(txthost.Text))
+            {
+                MessageBox.Show("El host es campo es requerido");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtuser.Text))
+            {
+                MessageBox.Show("El usuario es campo es requerido");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtpass.Text))
+            {
+                MessageBox.Show("La contraseña es campo es requerido");
+                return;
+            }
+
+            try
+            {
+                conexion = new MySqlConnection();
+                conexion.ConnectionString =
+                    "server=" + txthost.Text + ";"
+                    + "user id=" + txtuser.Text + ";"
+                    + "password=" + txtpass.Text + ";";
+                conexion.Open();
+                MessageBox.Show("La conexión se realizo exitosamente");
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ups hubo un error en la conexion :c");
+            }
+            finally
+            {
+                if (conexion.State == System.Data.ConnectionState.Open)
+                    conexion.Close();
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
